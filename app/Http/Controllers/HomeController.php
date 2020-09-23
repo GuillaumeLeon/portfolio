05732a3452945b21863468a8fc32eceeff785563
Guillaume Leon <guillaume.leon2000@gmail.com>
2020-09-24 00:40:27 +0200

    Cached the data to not cause problem with github api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
@@ -10,8 +11,13 @@ class HomeController extends Controller
 {
     public function index() {
 
-        $response = Http::get('https://api.github.com/users/GuillaumeLeon/repos');
-        $response = $response->json();
+        if (empty(Cache::get('response'))) {
+            $response = Http::get('https://api.github.com/users/GuillaumeLeon/repos');
+            $response = $response->json();
+            Cache::put('response', $response);
+        } else {
+            $response = Cache::get('response');
+        }
 
         return view('welcome', compact('response'));
     }
